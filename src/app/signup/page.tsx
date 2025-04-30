eb6e68d97e59efb8a4c0852d9d59053ba4bc9c48
Sharvari Dhote <dhotesharvari6@gmail.com>
2025-04-30 16:02:30 +0530

    push

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,10 +4,27 @@ import React from "react";
 import {useRouter} from "next/navigation";
 import {Axios} from "axios";
 
-export default function LoginPage() {
+export default function SignupPage() {
+    const [user,setUser] = React.useState({
+        email:"",
+        password:"",    
+        username:"",
+    })
+
+    const onSignup= async ()=>{
+    }
+
     return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <h1>Signup</h1>
+        <hr/>
+        <label htmlFor="username">Username</label>
+        <input 
+        className="border-2 border-gray-300 rounded-md p-2 m-2 text-black bg-gray-200"
+        type="text" id="username" 
+        value={user.username} 
+        onChange={(e)=>setUser({...user,username:e.target.value})} 
+        />
     </div>
     )
 }
