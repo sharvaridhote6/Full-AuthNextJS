10f75d4337c7a13e14644068f118fca97dff6809
Sharvari Dhote <dhotesharvari6@gmail.com>
2025-04-30 15:00:36 +0530

    initial push

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+"use client"; // this makes the page a client component
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+import {Axios} from "axios";
+
+export default function LoginPage() {
+    return (
+    <div>
+        <h1>Signup</h1>
+    </div>
+    )
+}
